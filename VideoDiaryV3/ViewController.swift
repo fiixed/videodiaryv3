bc68f3ce760086f0576cab5c334b39abb7188e54
Andrew Bell <fiixed@gmail.com>
2015-11-17 14:19:33 -0600

    Initial Commit

---
+++ VideoDiaryV3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoDiaryV3
+//
+//  Created by Andrew Bell on 11/17/15.
+//  Copyright © 2015 FiixedMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
